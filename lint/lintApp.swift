010bbb2e87858a330b5c6c09907924654c67f3e7
Lucas Portella <lucas.portella>
2020-09-14 22:17:30 -0300

    Initial Commit

---
+++ lint/lintApp.swift
@@ -0,0 +1,17 @@
+//
+//  lintApp.swift
+//  lint
+//
+//  Created by Lucas Portella on 14/09/20.
+//
+
+import SwiftUI
+
+@main
+struct lintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
