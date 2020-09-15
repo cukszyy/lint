09c8b3a4a7edc1ffa61c3c91459ad4d1e2c83fbf
Lucas Portella <lucas.portella>
2020-09-14 23:01:17 -0300

    Inject and configure Firebase dependency

---
+++ lint/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  lint
+//
+//  Created by Lucas Portella on 14/09/20.
+//
+
+import Foundation
+import Firebase
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions:
+            [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
+        return true
+      }
+    
+}
--- lint/lintApp.swift
+++ lint/lintApp.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 @main
 struct lintApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             ContentView()
