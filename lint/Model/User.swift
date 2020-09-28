efeb6b92fe817bd501f75fc75e38b05410b967ac
Lucas Portella <lucas.portella>
2020-09-27 21:53:18 -0300

    Fecth user info on Settings view

---
+++ lint/Model/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  lint
+//
+//  Created by Lucas Portella on 27/09/20.
+//
+
+import SwiftUI
+
+struct User {
+    var username: String
+    var bio: String
+    var avatar: String
+}
